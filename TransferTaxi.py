bc92d1a66a44096ce2cf2e34834c10486be4cd1d
John Blakkan <john1@blakkan.net>
2016-12-05 11:42:43 -0800

    typo in filename

--- TransferTaxi.py
+++ TransferTaxi.py
@@ -12,6 +12,6 @@ for company in ["yellow", "green", "fhv"]:
             os.system("wget https://s3.amazonaws.com/nyc-tlc/trip+data/%s --no-check-certificate --no-verbose" % s3_source_filename)
 
             os.system("tail -n +2 %s | python decimate.py 0.001 > %s" % (s3_source_filename, local_filename[0]))
-            os.remove(s3_source_file_name)
+            os.remove(s3_source_filename)
             #hdfs dfs -put y_201601.csv /user/w205/yellow
             #rm y_201601.csv
