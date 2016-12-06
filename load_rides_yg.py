c5f86e46ecdb9e7cda9370f9f1045c3247ea0081
Andrea Pope <andrealpope@gmail.com>
2016-12-05 22:14:10 -0500

    updated spacing and Load Rides App name

--- load_rides_yg.py
+++ load_rides_yg.py
@@ -6,19 +6,19 @@ from pyspark.sql import HiveContext
 
 def build_sql(ride_source, year, month):
     if ride_source == 'Yellow':
-	    str_sql = "CREATE TABLE tmp_rides_yg AS "
-	    str_sql += "SELECT 'Yellow' as ride_source, passenger_count, round(pickup_longitude,6) as pickup_longt, round(pickup_latitude,6) as pickup_lat, "
-	    str_sql += "year(tpep_pickup_datetime) as pickup_year, month(tpep_pickup_datetime) as pickup_month, dayofmonth(tpep_pickup_datetime) as pickup_day, "
-	    str_sql += "weekofyear(tpep_pickup_datetime) as pickup_week, from_unixtime(unix_timestamp(tpep_pickup_datetime),'EEEEE') as pickup_weekday, hour(tpep_pickup_datetime) as pickup_hour "
-	    str_sql += "FROM yellow_rides_schema "
-	    str_sql += "WHERE year(tpep_pickup_datetime) = " + str(year) + " and month(tpep_pickup_datetime) = " + str(month)
+		str_sql = "CREATE TABLE tmp_rides_yg AS "
+		str_sql += "SELECT 'Yellow' as ride_source, passenger_count, round(pickup_longitude,6) as pickup_longt, round(pickup_latitude,6) as pickup_lat, "
+		str_sql += "year(tpep_pickup_datetime) as pickup_year, month(tpep_pickup_datetime) as pickup_month, dayofmonth(tpep_pickup_datetime) as pickup_day, "
+		str_sql += "weekofyear(tpep_pickup_datetime) as pickup_week, from_unixtime(unix_timestamp(tpep_pickup_datetime),'EEEEE') as pickup_weekday, hour(tpep_pickup_datetime) as pickup_hour "
+		str_sql += "FROM yellow_rides_schema "
+		str_sql += "WHERE year(tpep_pickup_datetime) = " + str(year) + " and month(tpep_pickup_datetime) = " + str(month)
 	else:
 		str_sql = "CREATE TABLE tmp_rides_yg AS "
-	    str_sql += "SELECT 'Green' as ride_source, passenger_count, round(pickup_longitude,6) as pickup_longt, round(pickup_latitude,6) as pickup_lat,  "
-	    str_sql += "year(lpep_pickup_datetime) as pickup_year, month(lpep_pickup_datetime) as pickup_month, dayofmonth(lpep_pickup_datetime) as pickup_day,  "
-	    str_sql += "weekofyear(lpep_pickup_datetime) as pickup_week, from_unixtime(unix_timestamp(lpep_pickup_datetime),'EEEEE') as pickup_weekday, hour(lpep_pickup_datetime) as pickup_hour "
-	    str_sql += "FROM green_rides_schema "
-	    str_sql += "WHERE year(lpep_pickup_datetime) = " + str(year) + " and month(lpep_pickup_datetime) = " + str(month)
+		str_sql += "SELECT 'Green' as ride_source, passenger_count, round(pickup_longitude,6) as pickup_longt, round(pickup_latitude,6) as pickup_lat,  "
+		str_sql += "year(lpep_pickup_datetime) as pickup_year, month(lpep_pickup_datetime) as pickup_month, dayofmonth(lpep_pickup_datetime) as pickup_day,  "
+		str_sql += "weekofyear(lpep_pickup_datetime) as pickup_week, from_unixtime(unix_timestamp(lpep_pickup_datetime),'EEEEE') as pickup_weekday, hour(lpep_pickup_datetime) as pickup_hour "
+		str_sql += "FROM green_rides_schema "
+		str_sql += "WHERE year(lpep_pickup_datetime) = " + str(year) + " and month(lpep_pickup_datetime) = " + str(month)
 	return str_sql
 
 # other sql statement doesn't change:
@@ -35,7 +35,7 @@ str_sql2 += "pickup_week, pickup_weekday, pickup_hour "
 
 
 # start Spark and  Hive SQL contexts
-sc = SparkContext("local", "load rides")
+sc = SparkContext("local", "Load Rides App")
 hc = HiveContext(sc)
 
 for ride_source in ['Yellow', 'Green']:
